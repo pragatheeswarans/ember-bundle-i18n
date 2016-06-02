3240c91e4cb203ac7eaa36a48a4fd09c890fbb3a
kamalakannan.n <kamalakannan.n@zohocorp.com>
2016-06-02 18:25:26 +0530

    Merge translations in default locale into other locales
    Uses the translations in the default locale and merges it with
    translations in other locales.
    
    Other changes:
      - Replaces base broccoli-filter with broccoli-plugin as base class
      - Make I18NAssetBuilder take input tree after formatting from I18NAssetMerger
    
    TODOs
      - Allow default locale to be passed as option(now en)
      - Channelise output file prefix `MessageResources_` and allow to be changed via options

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
     // Add options here
+    'ember-bundle-i18n': {
+      inputPath: 'tests/dummy/app/i18n'
+    }
   });
 
   /*
